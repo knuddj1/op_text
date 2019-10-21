696a03818c4c42660af703965030f72908fef76b
Dean Knudson <knuddj1@student.op.ac.nz>
2019-10-22 00:54:17 +1300

    Documentation for models.py complete

--- op_text/__init__.py
+++ op_text/__init__.py
@@ -1,3 +1,3 @@
-from op_text.utils import *
+from op_text.utils import setup_dataloader, setup_optim, get_confidence_scores, calculate_accuracy, LabelConverter
 from op_text.processing import DataFetcher
-from op_text.modeling import Bert, Roberta, DistilBert
+from op_text.models import Bert, Roberta, DistilBert
