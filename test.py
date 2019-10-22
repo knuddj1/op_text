5e9f41294bbd205d087b1f5f79ce27dcf0671171
Dean Knudson (1000025498) <KNUDDJ1@student.op.ac.nz>
2019-10-22 15:38:20 +1300

    fixed testing path generation

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ from op_text.models import Bert, Roberta, DistilBert
 base_dir = r"F:\fine_tuned_models"
 
 bert_models_dir = os.path.join(base_dir, "bert models")
-bert_models = ['bert-base-uncased'] + [os.path.join(bert_models_dir, m) for m in os.listdir(bert_models_dir)]
+bert_models = ['bert-base-uncased'] + [os.path.join(bert_models_dir, m, "chkpt epochs=1") for m in os.listdir(bert_models_dir)]
 
 roberta_models_dir = os.path.join(base_dir, "roberta models")
-roberta_models = ['roberta-base'] + [os.path.join(roberta_models_dir, m) for m in os.listdir(roberta_models_dir)]
+roberta_models = ['roberta-base'] + [os.path.join(roberta_models_dir, m, "chkpt epochs=1") for m in os.listdir(roberta_models_dir)]
 
 distil_models_dir = os.path.join(base_dir, "distilbert models")
-distil_models = ['distilbert-base-uncased']+ [os.path.join(distil_models_dir, m) for m in os.listdir(distil_models_dir)]
+distil_models = ['distilbert-base-uncased']+ [os.path.join(distil_models_dir, m, "chkpt epochs=1") for m in os.listdir(distil_models_dir)]
 
 X_train = [
     "This is a test sentence",
@@ -24,18 +24,21 @@ y_train = [0, 0, 1, 1, 0]
 
 for m in bert_models:
     model = Bert(m)
-    model.fit(X_train, y_train, validation_split=0.4, chkpt_model_every=1, model_save_dir=os.getcwd(), nb_epoch=2) 
+    model_save = os.path.join(os.getcwd(), m.split("\\")[-2])
+    model.fit(X_train, y_train, validation_split=0.4, chkpt_model_every=1, model_save_dir=model_save, nb_epoch=2) 
     print(m, model.evaluate(X_train, y_train))
     print(m, model.predict(X_train[:1]))
 
 for m in roberta_models:
     model = Roberta(m)
-    model.fit(X_train, y_train, validation_split=0.4, chkpt_model_every=1, model_save_dir=os.getcwd(), nb_epoch=2)
+    model_save = os.path.join(os.getcwd(), m.split("\\")[-2])
+    model.fit(X_train, y_train, validation_split=0.4, chkpt_model_every=1, model_save_dir=model_save, nb_epoch=2)
     print(m, model.evaluate(X_train, y_train))
     print(m, model.predict(X_train[:1]))
 
 for m in distil_models:
     model = DistilBert(m)
-    model.fit(X_train, y_train, validation_split=0.4, chkpt_model_every=1, model_save_dir=os.getcwd(), nb_epoch=2)
+    model_save = os.path.join(os.getcwd(), m.split("\\")[-2])
+    model.fit(X_train, y_train, validation_split=0.4, chkpt_model_every=1, model_save_dir=model_save, nb_epoch=2)
     print(m, model.evaluate(X_train, y_train))
     print(m, model.predict(X_train[:1]))
