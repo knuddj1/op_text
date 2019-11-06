2d44af89327bd238f9310d18c5f32f04345121e0
Dean Knudson <knuddj1@student.op.ac.nz>
2019-11-07 00:56:58 +1300

    removed tqdm added verbose options

--- test.py
+++
@@ -1,9 +0,0 @@
-from op_text.models import DistilBert
-
-fp = r"C:\Users\The Baboon\Downloads\distilbert5labels"
-m = DistilBert(fp)
-
-t = ["Test"] * 100
-l = [1] * 100
-
-m.fit(t, l, nb_epoch=10, verbose=True)
